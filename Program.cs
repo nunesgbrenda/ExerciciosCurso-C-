93086f69b85680568a58a46d4229ecc6cc240d2f
nunesgbrenda <49684105+nunesgbrenda@users.noreply.github.com>
2021-06-26 12:44:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Digite dois valores inteiros para uma soma: ");
+            int n1;
+            int n2;
+            int soma; 
+
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            soma = n1 + n2;
+            Console.WriteLine("Soma= "+soma);
+        }
+    }
+}
